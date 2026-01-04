139eddf8772282764d6f98e697b57a85a9c2376a
JoepaulGospel <162193500+JoepaulGospel@users.noreply.github.com>
2026-01-04 17:13:24 +0100

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Taskplus Dashboard",
+  description: "Project Management Dashboard",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-background text-white antialiased overflow-hidden`}>
+        {children}
+      </body>
+    </html>
+  );
+}
